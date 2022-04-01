b9b4285bc735cb2041df2def0cd23df32e6ba10e
anega <anegamega@gmail.com>
2022-04-01 17:26:39 +0300

    Show Email, User models in the Admin

--- mail/admin.py
+++ mail/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from mail.models import User, Email
 
-# Register your models here.
+
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'user', 'read', 'archived', 'timestamp')
+
+
+admin.site.register(User)
+admin.site.register(Email, EmailAdmin)
